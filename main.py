545eecbd7e5021710e9f3d87a039c00eda36aabf
RogerHdzC <hdc.rogelio@gmail.com>
2024-04-06 12:22:10 -0600

    feat(main): added menu options to use the function add manually

--- main.py
+++ main.py
@@ -4,10 +4,28 @@ def main():
     archivo_json = "datos_cafeteria.json"
     cafeteria = Cafeteria(archivo_json)
 
-    # Mostrar las bebidas actuales antes de mostrar el menú
-    print("Bebidas actuales:")
-    for bebida, tamanos in cafeteria.bebidas.items():
-        print(f"{bebida}: {', '.join(map(str, tamanos))}")
+    while True:
+        # Mostrar las bebidas actuales antes de mostrar el menú
+        print("Bebidas actuales:")
+        for bebida, tamanos in cafeteria.bebidas.items():
+            print(f"{bebida}: {', '.join(map(str, tamanos))}")
+
+        # Mostrar el menú
+        print("\nMenú:")
+        print("1. Agregar nueva bebida")
+        print("2. Salir")
+
+        opcion = input("Ingrese su opción: ")
+
+        if opcion == "1":
+            entrada = input("Ingrese los detalles de la bebida (nombre, tamaños separados por comas): ")
+            resultado = cafeteria.agregar_bebida(entrada)
+            print(resultado)
+        elif opcion == "2":
+            print("Saliendo del programa...")
+            break
+        else:
+            print("Opción no válida. Por favor, ingrese 1 o 2.")
 
 if __name__ == "__main__":
     main()
