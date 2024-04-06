42e6c4afb844e774b5270dfa0e72bf4c596106c3
RogerHdzC <hdc.rogelio@gmail.com>
2024-04-06 12:25:57 -0600

    feat(Cafeteria): added type value verification

--- Cafeteria.py
+++ Cafeteria.py
@@ -26,9 +26,13 @@ class Cafeteria:
         if len(partes) < 2:
             return "Error: Formato de entrada inválido."  # No hay suficientes partes después de dividir por comas
         
-        nombre = partes[0].strip()  
-        tamanos = partes[1].split(',')
-        tamanos = list(map(int, tamanos))
+        # Verificar los tamaños
+        try:
+            nombre = partes[0].strip().lower()  # Convertir el nombre a minúsculas
+            tamanos = partes[1].split(',')
+            tamanos = list(map(int, tamanos))
+        except ValueError:
+            return "Error: Los tamaños deben ser valores enteros dentro del rango de 1 a 48 y deben estar en orden ascendente."
         
         # Agregar la bebida al diccionario de bebidas
         self.bebidas[nombre] = tamanos
